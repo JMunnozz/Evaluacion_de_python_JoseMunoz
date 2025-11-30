f44ed2ef037c3531781b80d05a5e256a803771b1
Jmunsan <158281056+JMunnozz@users.noreply.github.com>
2025-11-30 03:04:29 +0100

    importar_tests_unittest

---
+++ tests/suite_uniTest/test_ejercicio4_JoseMunoz.py
@@ -0,0 +1,9 @@
+#Test realizado por Jose Muñoz Sánchez
+
+import sys
+
+sys.path.insert(0, '../../src/ejercicio2')
+sys.path.insert(1, '../../src/ejercicio3')
+
+from ejercicio2.binario import esBinario
+from ejercicio3.lista import estaEnRango, estaEnLista
