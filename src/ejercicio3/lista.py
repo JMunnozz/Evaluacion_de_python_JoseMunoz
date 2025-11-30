d292ef441f1c504e70178fb3bf5f22a6002f2b1c
Jmunsan <158281056+JMunnozz@users.noreply.github.com>
2025-11-30 02:17:31 +0100

    ejercicio_3_con__funciones

--- src/ejercicio3/lista.py
+++ src/ejercicio3/lista.py
@@ -18,14 +18,27 @@ usuarios = {
  }
  }
 
+def estaEnRango(valor, minimo, maximo):
+    if( valor >= minimo and valor <= maximo):
+         return True
+    else:
+         return False
+
+def estaEnLista(valor, lista):
+     if(valor in lista):
+          return True
+     else:
+          return False
+
+#Funciones creadas por José Muñoz Sánchez
 
 intentos = 0
 
-while intentos < 3:
+while estaEnRango(intentos, 0, 2):
     preguntaNombre = input("Introduce un nombre: ")
     preguntaContraseña = input("Introduce la contraseña del usuario: ")
 
-    if preguntaNombre in usuarios:
+    if estaEnLista(preguntaNombre, usuarios):
         if preguntaContraseña == usuarios[preguntaNombre]["password"]:
             print("Has iniciado sesión correctamente. Tu nombre es "+usuarios[preguntaNombre]["nombre"]+", tu apellido "+usuarios[preguntaNombre]["apellido"]+" y mi nombre José Muñoz")
             break
